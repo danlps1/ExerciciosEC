4301af88829905abd980fd7a617f1961bd5c906a
Daniel <danieloliv.contato@gmail.com>
2025-05-25 11:21:56 -0300

    GradeService Testes

--- exercicio#4/src/services/grade/grade.service.spec.ts
+++ exercicio#4/src/services/grade/grade.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GradeService } from './grade.service';
+import { GradeDto } from '../../dto/grade.dto';
+import { MateriaEntity } from '../../entity/materia.entity';
+import { GradeEntity } from '../../entity/grade.entity';
 
-describe('GradeService', () => {
-  let service: GradeService;
+describe('GradeService Testes', () => {
+  let gradeService: GradeService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [GradeService],
     }).compile();
 
-    service = module.get<GradeService>(GradeService);
+    gradeService = module.get<GradeService>(GradeService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(gradeService).toBeDefined();
+  });
+
+  it('deve criar uma grade com sucesso', async () => {
+    const dto: GradeDto = { materiasId: [1, 2, 3, 4, 5] };
+
+    const materias = dto.materiasId.map((id) => {
+      const materia = new MateriaEntity();
+      materia.id = id;
+      return materia;
+    });
+
+    jest.spyOn(MateriaEntity, 'find').mockResolvedValue(materias);
+
+    const grade = new GradeEntity();
+    grade.id = 1;
+    grade.materias = materias;
+
+    jest.spyOn(GradeEntity, 'save').mockResolvedValue(grade);
+
+    const resultado = await gradeService.criarGrade(dto);
+
+    expect(resultado).toEqual(grade);
+  });
+
+  it('deve lançar erro se tiver menos de 5 matérias', async () => {
+    const dto: GradeDto = { materiasId: [1, 2, 3] };
+
+    await expect(gradeService.criarGrade(dto)).rejects.toThrow(
+      'Uma grade deve ter no minimo 5 matérias!',
+    );
   });
 });
